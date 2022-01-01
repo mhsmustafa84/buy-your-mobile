a174d57c061d25de95ed4b6748e9f95ec72543bd
Mostafa AbdElHamid <mhsmustafa84@gmail.com>
2022-01-01 18:01:09 +0200

    first commit

---
+++ Scripts/custom.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+
+    //adjust slider height
+
+    var windowHeight = $(window).height(),
+        navBarHeight = $('.navbar').innerHeight();
+
+    $('.slider .carousel-item').height(windowHeight - navBarHeight);
+
+    $('.carousel').carousel({
+        interval: 4000
+    })
+
+A
+
+
+
+});
